bba6a622406f5671567ec587d20782e020a81315
anjaraaa21 <anjarafanantenana175@gmail.com>
2025-07-08 08:55:44 +0300

    Update ClientController.php

--- ws/controllers/ClientController.php
+++ ws/controllers/ClientController.php
@@ -1,1 +1,14 @@
+<?php
+require_once __DIR__ . '/../models/Client.php';
+
+class ClientController {
+    public static function getAll() {
+        Flight::json(Client::getAll());
+    }
+
+    public static function create() {
+        $id = Client::create(Flight::request()->data);
+        Flight::json(['message' => 'Client ajouté', 'id' => $id]);
+    }
+}
 
