b875ee15cdb8b0cca5df0ae5cd7b8ac84f3c7126
anjaraaa21 <anjarafanantenana175@gmail.com>
2025-07-08 08:56:19 +0300

    Update Client.php

--- ws/models/Client.php
+++ ws/models/Client.php
@@ -1,1 +1,16 @@
+<?php
+require_once __DIR__ . '/../db.php';
+
+class Client {
+    public static function getAll() {
+        return getDB()->query("SELECT * FROM client")->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function create($data) {
+        $db = getDB();
+        $stmt = $db->prepare("INSERT INTO client (nom, prenom) VALUES (?, ?)");
+        $stmt->execute([$data->nom, $data->prenom]);
+        return $db->lastInsertId();
+    }
+}
 
